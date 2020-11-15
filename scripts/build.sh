4a6f443fad638fa8095ca108f347d2e3867f1bbf
Pong <samfunghp@gmail.com>
2020-11-15 23:54:20 +0800

    testing

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 yarn node-sass \
+  --watch \
   --importer node_modules/node-sass-package-importer/dist/cli.js \
   --functions scripts/sass-custom-functions.js \
   --output dist  \
