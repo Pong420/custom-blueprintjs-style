5f1ea32ffc962e8c431a2154da1e38c725d440d6
Pong <samfunghp@gmail.com>
2020-11-16 22:21:41 +0800

    fix: scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 yarn node-sass \
-  --watch \
   --importer node_modules/node-sass-package-importer/dist/cli.js \
   --functions scripts/sass-custom-functions.js \
-  --output dist  \
-  --recursive src
+  --output dist \
+  --recursive src \
+  $1 \
